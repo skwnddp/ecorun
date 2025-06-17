eb97e60d2eb0410acda10d7ff5995515ea4308e9
kmj <7883121@naver.com>
2025-06-16 21:51:11 -0700

    테스트

---
+++ ecorun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ecorun
+//
+//  Created by mac on 2025/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        깃 테스트
+    }
+
+
+}
+
